d5471ceb95176820c2d3237f8067ad87aa0e8a4f
advikasingh <133577701+advikasingh@users.noreply.github.com>
2023-05-14 20:49:12 +0800

    area of rectangle

--- test.py
+++ test.py
@@ -2,3 +2,10 @@
 
 
 # input integrt value
+l = float(input("enter the value of length of rectangle"))
+b = float(input("enter the value of breadth of rectangle"))
+
+
+a = l*b
+
+print("area is", a)
